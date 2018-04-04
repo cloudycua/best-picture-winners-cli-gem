84792e52547babb77d0a3f3f12b75c8da1ab61c1
Christine Cua <cloudycua@gmail.com>
2018-04-04 12:44:09 +0000

    add rotten tomatoes urls

--- lib/best_picture_winners/scraper.rb
+++ lib/best_picture_winners/scraper.rb
@@ -1,9 +1,17 @@
 class BestPictureWinners::Scraper
 
-
   def get_page_rotten_tomatoes
   # https://editorial.rottentomatoes.com/guide/oscars-best-and-worst-best-pictures/
   # https://blog.roku.com/best-picture-oscar-winners-ranked-rotten-tomatoes
+    doc9 = Nokogiri::HTML(open("https://editorial.rottentomatoes.com/guide/oscars-best-and-worst-best-pictures/9/"))
+    doc8 = Nokogiri::HTML(open("https://editorial.rottentomatoes.com/guide/oscars-best-and-worst-best-pictures/8/"))
+    doc7 = Nokogiri::HTML(open("https://editorial.rottentomatoes.com/guide/oscars-best-and-worst-best-pictures/7/"))
+    doc6 = Nokogiri::HTML(open("https://editorial.rottentomatoes.com/guide/oscars-best-and-worst-best-pictures/6/"))
+    doc5 = Nokogiri::HTML(open("https://editorial.rottentomatoes.com/guide/oscars-best-and-worst-best-pictures/5/"))
+    doc4 = Nokogiri::HTML(open("https://editorial.rottentomatoes.com/guide/oscars-best-and-worst-best-pictures/4/"))
+    doc3 = Nokogiri::HTML(open("https://editorial.rottentomatoes.com/guide/oscars-best-and-worst-best-pictures/3/"))
+    doc2 = Nokogiri::HTML(open("https://editorial.rottentomatoes.com/guide/oscars-best-and-worst-best-pictures/2/"))
+    doc1 = Nokogiri::HTML(open("https://editorial.rottentomatoes.com/guide/oscars-best-and-worst-best-pictures/"))
   end
 
   def get_page_imdb
