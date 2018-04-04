57ad75b4cc5accb9642648be7714dd617cdfd0f4
Christine Cua <cloudycua@gmail.com>
2018-04-04 20:05:26 +0000

    working on scraper_info and movie

--- lib/best_picture_winners/movie.rb
+++ lib/best_picture_winners/movie.rb
@@ -4,8 +4,15 @@ class BestPictureWinners::Movie
   @@all = []
 
   def initialize(movie_hash)
+    attributes = [:title, :year, :url, :tomatometer, :audience_score, :genre, :runtime, :synopsis, :critic_consensus]
     movie_hash.each do |attribute, value|
       self.send("#{attribute}=", value)
+    end
+    attributes.each do |attribute, value|
+      case self[attribute]
+      when value == nil
+        self.send("#{attribute}=", "")
+      end
     end
     @@all << self
   end
---
+++ lib/best_picture_winners/scraper_info.rb
@@ -0,0 +1,19 @@
+class BestPictureWinners::ScraperInfo
+
+  def get_movie_page_from_url
+#    BestPictureWinners::Movie.all.each do |movie|
+#      movie_page = Nokogiri::HTML(open(movie.url))
+#      movie[:critic_consensus] = movie_page.css("div#scorePanel .col-full-xs .critic_consensus").text
+
+      movie_page = Nokogiri::HTML(open(BestPictureWinners::Movie.all[0].url))
+      critics = movie_page.css("div#scorePanel .col-full-xs p.critic_consensus").text.gsub("Critic Consensus:","").lstrip
+#      BestPictureWinners::Movie.all[0][:critic_consensus] = critics
+#      BestPictureWinners::Movie.all[0][:critic_consensus] = movie_page.css("div#scorePanel .col-full-xs .critic_consensus").text
+#      BestPictureWinners::Movie.all[0]
+BestPictureWinners::Movie.all[0].critics_consensus = critics
+BestPictureWinners::Movie.all[0].critics_consensus
+
+#    end
+  end
+
+end
