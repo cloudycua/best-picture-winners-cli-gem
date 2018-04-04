25a8f6865bd974ad119050de13fb496cbd291d8b
Christine Cua <cloudycua@gmail.com>
2018-04-04 00:38:59 +0000

    stub out

---
+++ lib/best_picture_winners/movie.rb
@@ -0,0 +1,12 @@
+# https://editorial.rottentomatoes.com/guide/oscars-best-and-worst-best-pictures/
+# https://m.imdb.com/chart/bestpicture/?ref_=m_nv_ch_osc
+# https://blog.roku.com/best-picture-oscar-winners-ranked-rotten-tomatoes
+
+class BestPictureWinners::movie
+  attr_accessor :name, :year, :imdb_rating, :rotten_tomatoes_rating, :critic_consensus, :synopsis, :starring, :director
+
+
+end
+
+
+end
---
+++ lib/best_picture_winners/scraper.rb
@@ -0,0 +1,15 @@
+class BestPictureWinners::Scraper
+
+  def get_page_rotten_tomatoes
+
+  end
+
+  def get_page_imdb
+
+  end
+
+  def make_movies
+
+  end
+
+end
