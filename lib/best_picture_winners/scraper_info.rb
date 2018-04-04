0c7b22200084ed0e1be3397a5c762761c233259a
Christine Cua <cloudycua@gmail.com>
2018-04-04 21:36:56 +0000

    working on genre for scraper_info

--- lib/best_picture_winners/scraper_info.rb
+++ lib/best_picture_winners/scraper_info.rb
@@ -7,9 +7,17 @@ class BestPictureWinners::ScraperInfo
 
       movie_page = Nokogiri::HTML(open(BestPictureWinners::Movie.all[0].url))
       BestPictureWinners::Movie.all[0].critic_consensus = movie_page.css("div#scorePanel .col-full-xs p.critic_consensus").text.gsub("Critic Consensus:","").lstrip
-#      BestPictureWinners::Movie.all[0][:critic_consensus] = critics
-#      BestPictureWinners::Movie.all[0][:critic_consensus] = movie_page.css("div#scorePanel .col-full-xs .critic_consensus").text
-#      BestPictureWinners::Movie.all[0]
+      BestPictureWinners::Movie.all[0].audience_score = movie_page.css(".audience-score .meter-value").text.lstrip
+      BestPictureWinners::Movie.all[0].tomatometer = movie_page.css("#all-critics-numbers .critic-score .meter-value").text.lstrip
+      BestPictureWinners::Movie.all[0].synopsis = movie_page.css("#movieSynopsis").text.lstrip
+      movie_page.css("li .meta-label").each do |category|
+        binding.pry
+        if category.text = "Genre:"
+          BestPictureWinners::Movie.all[0].genre = category.css(".meta-value").text
+        end
+      end
+
+      BestPictureWinners::Movie.all[0].genre
 
 #    end
   end
