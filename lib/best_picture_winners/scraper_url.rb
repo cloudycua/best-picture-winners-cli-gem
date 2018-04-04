150171e56c4592c2f8a537e712e55c7cccbcab41
Christine Cua <cloudycua@gmail.com>
2018-04-04 15:58:09 +0000

    update scrape_movie_titles

--- lib/best_picture_winners/scraper_url.rb
+++ lib/best_picture_winners/scraper_url.rb
@@ -3,8 +3,7 @@ class BestPictureWinners::ScraperURL
   def get_url_pages
     all_url_pages = []
     all_url_pages << Nokogiri::HTML(open("https://editorial.rottentomatoes.com/guide/oscars-best-and-worst-best-pictures/9/"))
-
-#    @all_url_pages << Nokogiri::HTML(open("https://editorial.rottentomatoes.com/guide/oscars-best-and-worst-best-pictures/8/"))
+    all_url_pages << Nokogiri::HTML(open("https://editorial.rottentomatoes.com/guide/oscars-best-and-worst-best-pictures/8/"))
 #    @all_url_pages << Nokogiri::HTML(open("https://editorial.rottentomatoes.com/guide/oscars-best-and-worst-best-pictures/7/"))
 #    @all_url_pages << Nokogiri::HTML(open("https://editorial.rottentomatoes.com/guide/oscars-best-and-worst-best-pictures/6/"))
 #    @all_url_pages << Nokogiri::HTML(open("https://editorial.rottentomatoes.com/guide/oscars-best-and-worst-best-pictures/5/"))
@@ -16,24 +15,25 @@ class BestPictureWinners::ScraperURL
   end
 
   def scrape_movie_titles
-    movie_title = get_url_pages[0].css(".article_movie_title h2 a").text
-    movie_title
+    movie_titles = []
+    count = get_url_pages.size
+    get_url_pages[0].css(".article_movie_title").each do |movie|
+      movie_titles << movie.css("h2 a").text
+    end
+    count
+  end
+
+#  def scrape_movie_urls
+#    movie_url = get_url_pages[0].css(".article_movie_title a h2").text
+#    movie_url
 
     # h2".attr('href'))
     #movie_url
-  end
+#  end
 
-  def scrape_movie_urls
-    movie_url = get_url_pages[0].css(".article_movie_title a h2").text
-    movie_url
-
-    # h2".attr('href'))
-    #movie_url
-  end
-
-  def make_movies
-    get
-  end
+#  def make_movies
+#    get
+#  end
 
 
 end
