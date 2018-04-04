2b8004fe3fe145781285ea63fbd112eb95b80e75
Christine Cua <cloudycua@gmail.com>
2018-04-04 01:57:17 +0000

    added urls

--- lib/best_picture_winners/scraper.rb
+++ lib/best_picture_winners/scraper.rb
@@ -1,11 +1,13 @@
 class BestPictureWinners::Scraper
 
-  def get_page_rotten_tomatoes
 
+  def get_page_rotten_tomatoes
+  # https://editorial.rottentomatoes.com/guide/oscars-best-and-worst-best-pictures/
+  # https://blog.roku.com/best-picture-oscar-winners-ranked-rotten-tomatoes
   end
 
   def get_page_imdb
-
+  # https://m.imdb.com/chart/bestpicture/?ref_=m_nv_ch_osc
   end
 
   def make_movies
