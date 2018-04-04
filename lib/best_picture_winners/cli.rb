83b7caa9fab4092da01bd16b92268dce471a2f23
Christine Cua <cloudycua@gmail.com>
2018-04-04 02:31:20 +0000

    start stubbing out cli

---
+++ lib/best_picture_winners/cli.rb
@@ -0,0 +1,29 @@
+class BestPictureWinners::cli
+
+  def call
+    start
+  end
+
+  def start
+    # Welcome
+    # How would you like the Best Picture winners to be sorted?
+    # 1: alphabetically
+    #    All movies or movies that start with a particular letter
+    # 2: by year
+    #    If by year, ask for all years or a range of years
+    # 3: by imdb_rating
+    #    Ask for all movies, above a certain rating, or top X movies
+    # 4: by rotten_tomatoes_rating
+    #    Ask for all movies, above a certain rating, or top X movies
+    # validate entry else re-enter
+    # Ask if user would like the list to include a synopsis of the movie
+    # Ask user if they would like the list to include the critic_consensus
+    # 1-print list alphabetically with or without synopsis or critic_consensus
+    # 2-print list by year with or without synopsis or critic_consensus
+    # 3-print list by imdb_rating with or without synopsis or critic_consensus
+    # 4-print list by rotten_tomatoes_rating with or without synopsis or critic_consensus
+    # Ask if user would like to see another list
+    # Exit
+  end
+
+end
