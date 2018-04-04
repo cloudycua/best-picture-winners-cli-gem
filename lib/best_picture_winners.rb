d1e6d73d8eff9f6636f9b87d96be0c15c918da74
Christine Cua <cloudycua@gmail.com>
2018-04-04 00:21:37 +0000

    created environment

--- lib/best_picture_winners.rb
+++ lib/best_picture_winners.rb
@@ -1,4 +1,10 @@
-require "best_picture_winners/version"
+require 'open-uri'
+require 'pry'
+require 'nokogiri'
+
+require_relative "best_picture_winners/movie"
+require_relative "best_picture_winners/cli"
+require_relative "best_picture_winners/version"
 
 module BestPictureWinners
   # Your code goes here...
